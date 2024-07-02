7088264e5358ed6150d372a50fd48f123a26ae93
Shahmeer-09 <shahmeerweb@gmail.com>
2024-07-02 12:18:19 +0500

    .

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
   credentials: true,
 };
-console.log(path.resolve(__dirname, "./client/dist", "index.html"))
+console.log(path.resolve(__dirname, "./client/dist/index.html"))
 app.use(cors(corsOptions));
 app.use(express.static(path.resolve(__dirname, ".")));
 app.use(express.json({ limit: "50mb" }));
